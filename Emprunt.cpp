7f9babba36308f6f5007e77791b4030a8aabee19
stoura <amriwael149@gmail.com>
2025-12-08 17:14:08 +0100

    Question loula cbon

--- Emprunt.cpp
+++ Emprunt.cpp
@@ -1,1 +1,29 @@
 #include "Emprunt.h"
+#include "Date.h"
+#include <string>
+#include <vector>
+Emprunt::Emprunt(const Date& dateEmprunt, int ISBN, int ID) : dateEmprunt_(dateEmprunt),ISBN_(ISBN),ID_(ID) {}
+
+void Emprunt::setISBN(const int& ISBN) {
+	ISBN_ = ISBN; 
+};
+
+void Emprunt::setID(const int& ID) {
+	ID_ = ID;
+};
+
+void Emprunt::setDate(const Date& dateEmprunt) {
+	dateEmprunt_ = dateEmprunt;
+};
+
+const int& Emprunt::isbn() const {
+	return ISBN_;
+};
+
+const int& Emprunt::id() const {
+	return ID_;
+};
+
+const Date& Emprunt::dateEmprunt() const {
+	return dateEmprunt_;
+};
--- Emprunt.h
+++ Emprunt.h
@@ -1,5 +1,20 @@
 #pragma once
+#include "Date.h"
+#include <string>
+#include <vector>
 class Emprunt
 {
+public:
+	Date dateEmprunt_;
+	int ISBN_;
+	int ID_;
+private:
+	Emprunt(const Date& dateEmprunt, int ISBN, int ID);
+	void setISBN(const int& ISBN);
+	void setID(const int& ID);
+	void setDate(const Date& dateEmprunt);
+	const int& isbn() const;
+	const int& id() const;
+	const Date& dateEmprunt() const;
 };
 
